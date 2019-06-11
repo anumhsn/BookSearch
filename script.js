e789409c75b2d536eb5d2cf988ec56a54fa07b42
anumhsn22 <anumh123@gmail.com>
2019-06-11 09:09:56 -0400

    increased styling of output

--- script.js
+++ script.js
@@ -1,74 +1,78 @@
 const titleBtn = document.querySelector("#getTitle");
 const authorBtn = document.querySelector("#getAuthor");
 const input = document.querySelector("input");
-const showInfo = document.querySelector("p");
+const showInfo = document.querySelector(".content");
 const ISBNbtn = document.querySelector("#getISBN");
 const API_KEY = "AIzaSyCrIuS7E-4mpw-1wLknE4C30W3_Cb_iGBc";
-maxResults=40;
 
 titleBtn.addEventListener("click", async () => {
-   // getBookTitle();
-   showInfo.innerHTML = '';
-    let inpt = input.value; 
+    // getbooksArr();
+    showInfo.innerHTML = '';
+    let inpt = input.value;
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${inpt}&key=${API_KEY}&maxResults=40`)
-    const bookTitle = response.data.items;
-    for (let i=0; i<bookTitle.length; i +=1){
+    const booksArr = response.data.items;
+    for (let i = 0; i < booksArr.length; i += 1) {
+        const bookCard = document.createElement('div');
+        bookCard.innerHTML = `
+        <img src= ${booksArr[i].volumeInfo.imageLinks.smallThumbnail} /> <br>
+        <li>${booksArr[i].volumeInfo.title}</li> <br>
         
-        showInfo.innerHTML += `<li>${bookTitle[i].volumeInfo.title}</li>
-        <img src= ${bookTitle[i].volumeInfo.imageLinks.smallThumbnail} />
         <button id ="moreInfo">Click here for more info!</button>
         `
-        const moreInfo = document.querySelector("#moreInfo");
-        
-        moreInfo.addEventListener("click", function(){
-            document.location.href = bookTitle[i].volumeInfo.infoLink;
-        })
-    }
-       
+        const moreInfo = bookCard.querySelector("#moreInfo");
 
-    console.log(bookTitle);
+        moreInfo.addEventListener("click", function () {
+            location.href = booksArr[i].volumeInfo.infoLink;
+        })
+        showInfo.appendChild(bookCard);
+    }
+    console.log(booksArr);
 })
 
 
 authorBtn.addEventListener("click", async () => {
     showInfo.innerHTML = '';
-    const inpt = input.value; 
+    const inpt = input.value;
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${inpt}&key=${API_KEY}`)
-    const bookAuthor = response.data.items;
-    for (let i=0; i<bookAuthor.length; i +=1){
-        showInfo.innerHTML += `<li>
-        ${bookAuthor[i].volumeInfo.authors} , ${bookAuthor[i].volumeInfo.title}</li>
-        <img src= ${bookAuthor[i].volumeInfo.imageLinks.smallThumbnail} />
+    const authorArr = response.data.items;
+    for (let i = 0; i < authorArr.length; i += 1) {
+        const bookCard = document.createElement('div');
+        bookCard.innerHTML = `<li>
+        ${authorArr[i].volumeInfo.authors} , ${authorArr[i].volumeInfo.title}</li>
+        <img src= ${authorArr[i].volumeInfo.imageLinks.smallThumbnail} />
         <button id ="moreInfo">Click here for more info!</button>
         `
-        const moreInfo = document.querySelector("#moreInfo");
-        
-        moreInfo.addEventListener("click", function(){
-            document.location.href = bookAuthor[i].volumeInfo.infoLink;
+        const moreInfo = bookCard.querySelector("#moreInfo");
+
+        moreInfo.addEventListener("click", function () {
+            location.href = authorArr[i].volumeInfo.infoLink;
         })
+        showInfo.appendChild(bookCard);
     }
-    //console.log(bookTitle);
+    //console.log(booksArr);
 })
 
 ISBNbtn.addEventListener("click", async () => {
     showInfo.innerHTML = '';
-    const inpt = input.value; 
+    const inpt = input.value;
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${inpt}&key=${API_KEY}&maxResults=40`)
     const ISBNinfo = response.data.items;
-    for (let i=0; i<ISBNinfo.length; i +=1){
-        showInfo.innerHTML += `<li>
+    for (let i = 0; i < ISBNinfo.length; i += 1) {
+        const bookCard = document.createElement('div');
+        bookCard.innerHTML = `<li>
         
          ${ISBNinfo[i].volumeInfo.title}</li>
         <img src= ${ISBNinfo[i].volumeInfo.imageLinks.smallThumbnail} />
         <button id ="moreInfo">Click here for more info!</button>
         `
-        const moreInfo = document.querySelector("#moreInfo");
-        
-        moreInfo.addEventListener("click", function(){
-            document.location.href = ISBNinfo[i].volumeInfo.infoLink;
+        const moreInfo = bookCard.querySelector("#moreInfo");
+
+        moreInfo.addEventListener("click", function () {
+            location.href = ISBNinfo[i].volumeInfo.infoLink;
         })
+        showInfo.appendChild(bookCard);
     }
-    //console.log(bookTitle);
+    //console.log(booksArr);
 })
 
 
